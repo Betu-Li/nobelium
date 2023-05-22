2062d13d6a8dfb0276deaa16f25015b728d3442e
YuaYagira <58613968+YuayYeonhi@users.noreply.github.com>
2023-05-22 17:02:37 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-title: 'CRAIGARY',
+title: 'GDADMN',
 author: 'Yuay',
-email: 'yuayagira@outlook.com',
-link: 'https://notion.yuay.com.cn',
-description: 'This gonna be an awesome website!',
+email: '2831352144@qq.com',
+link: 'https://yuay.ac.cn',
+description: 'Game Design And Development Manuscript Notes',
 lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
 timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
 appearance: 'auto', // ['light', 'dark', 'auto'],
@@ -18,9 +18,9 @@ showAbout: true,
 showArchive: true,
 autoCollapsedNavBar: false, // The automatically collapsed navigation bar
 ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-socialLink: 'https://twitter.com/craigaryhart',
+socialLink: 'https://yuay.flowus.cn/',
 seo: {
-keywords: ['Blog', 'Website', 'Notion'],
+keywords: ['Blog', 'Yuay', 'Game'],
 googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
 },
 notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
