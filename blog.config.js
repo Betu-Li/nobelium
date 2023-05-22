09a340507ee2717d6fac6eae8ed7130d121aae98
YuaYagira <58613968+YuayYeonhi@users.noreply.github.com>
2023-05-22 17:40:26 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ author: 'Yuay',
 email: '2831352144@qq.com',
 link: 'https://yuay.ac.cn',
 description: 'Game Design And Development Manuscript Notes',
-lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
 timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
 appearance: 'auto', // ['light', 'dark', 'auto'],
 font: 'sans-serif', // ['sans-serif', 'serif']
